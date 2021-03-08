1e41144bab9e6cbdbaf4964a36fbe9b26e94f6b2
Miracleyoo <miracleyoo@163.com>
2021-03-07 21:14:29 -0500

    update main

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+# Copyright 2021 Zhongyang Zhang
+# Contact: mirakuruyoo@gmai.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """ This main entrance of the whole project.
 
     Most of the code should not be changed, please directly
     add all the input arguments of your model's constructor
     and the dataset file's constructor. The MInterface and 
-    DInterface can be seen as transparent to all your args.
-
-    Author : Zhongyang Zhang
-    Contact: mirakuruyoo@gmai.com
+    DInterface can be seen as transparent to all your args.    
 """
 import pytorch_lightning as pl
 from argparse import ArgumentParser
